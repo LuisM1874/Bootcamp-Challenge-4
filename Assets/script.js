da9b2db60ad7443897ca63e54b935dd082ad3e88
Luis Moore <luismoore@Luiss-MacBook-Air.local>
2023-12-07 15:02:07 +0000

    added JS questions

--- Assets/script.js
+++ Assets/script.js
@@ -1,3 +1,34 @@
+// Questions that will be asked 
+var questions = [
+    {
+        prompt : " What does a file containing Javascript end in?",
+        options : [".js",".css",".html","It can be put in any"],
+        answer : ".js"
+
+    },
+    {
+        prompt : "What is the correct syntax for referring to an external script called 'xxx.js'?",
+        options : ["<script src = 'xxx.js>","<script name = 'xxx.js'","<script href = 'xxx.js'"],
+        answer : "<script src = 'xxx.js'>"
+    },
+    {
+        prompt : "The external JavaScript file must contain the <script> tag.",
+        options : ["True","False"],
+        answer :"False"
+    },
+    {
+        prompt : "How does a FOR loop start",
+        options : ["for (i = 0; i <= 5)", "for (i = 0; i <= 5; i++)", "for (i = 5)", "for (i = 0; i++)"],
+        answer : "for (i = 0; i <= 5; i++)"
+    },
+    {
+        prompt : "Javascript is the same as Java",
+        options : ["True","False"],
+        answer : "False",
+    },
+];
+
+
 
 
 
